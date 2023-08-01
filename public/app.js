656adc72a3ef79c0689c9b04bf3e64deff2f4a44
Antonio Navas <antonioluis_7_@hotmail.com>
2023-08-01 18:03:29 +0200

    "a ver si conseguimos que tire"

--- public/app.js
+++ public/app.js
@@ -2,28 +2,20 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-app.get("/Home", (req, res, next) => {
-    res.send(__dirname + "/views/Home.html")
+app.get("/views/home", (req, res, next) => {
+    res.send(__dirname + "/views/home.html")
 })
 
-// app.get("/home", (req, res, next) => {
-//     // ruta que enviará al usuario una pagina
-  
-//     // sendFile para enviar archivos
-//     console.log( __dirname ) // la direccion absoluta de esta ubicación
-//     res.sendFile(__dirname + "/views/index.html")
-//   })
-
-app.get("/About", (req, res, next) => {
-    res.send(__dirname + "/views/About.html")
+app.get("/views/about", (req, res, next) => {
+    res.send(__dirname + "/views/about.html")
 })
-app.get("/Works", (req, res, next) => {
-    res.send(__dirname + "/views/Works.html")
+app.get("/views/works", (req, res, next) => {
+    res.send(__dirname + "/views/works.html")
 })
-app.get("/Photo-Gallery", (req, res, next) => {
-    res.send(__dirname + "/views/Photo-Gallery.html")
+app.get("/views/photo-Gallery", (req, res, next) => {
+    res.send(__dirname + "/views/photo-Gallery.html")
 })
 
 app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`)
+    console.log(`App listening on port ${port}`)
 })
